ada7a1227dd9c6e1f90dce1166e78ebc57ad053e
suxrobshukurov <sdead96@gmail.com>
2022-02-28 18:37:06 +0300

    added new block

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,10 @@ gulp.task('css', async function() {
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/animate.css/animate.min.css',
+            'node_modules/hover.css/css/hover-min.css',
+            'node_modules/swiper/swiper-bundle.min.css',
+            'node_modules/@fancyapps/ui/dist/fancybox.css',
         ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('src/scss'))
@@ -55,7 +59,11 @@ gulp.task('script', async function() {
 gulp.task('js', async function() {
     return gulp.src([
             'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/slick-carousel/slick/slick.js'
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/wowjs/dist/wow.min.js',
+            'node_modules/swiper/swiper-bundle.min.js',
+            'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+            'node_modules/jquery.maskedinput/src/jquery.maskedinput.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 jQuery(function() {
-    console.log("main js work!");
-
-
+    let swiper = new Swiper(".mySwiper", {
+        grabCursor: true,
+        effect: "cards",
+        loop: false
+    });
 });
+
+ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
